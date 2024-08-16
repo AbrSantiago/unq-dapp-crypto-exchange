c57be15adce22191ff31e7d23fef5e587ea9b4c7
Santi <santiago_abregu_@hotmail.com>
2024-08-15 23:17:19 -0300

    Setup Ready

---
+++ src/test/java/ar/edu/unq/dapp_api/DappApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.dapp_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DappApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
