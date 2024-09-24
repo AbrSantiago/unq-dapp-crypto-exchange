39604b813850e01e540183a39208a7a7648451b2
Santiago <santiago_abregu_@hotmail.com>
2024-09-24 15:44:32 -0300

    Removed unused import

--- src/test/java/ar/edu/unq/dapp_api/DappApiApplicationTests.java
+++ src/test/java/ar/edu/unq/dapp_api/DappApiApplicationTests.java
@@ -1,14 +1,11 @@
 package ar.edu.unq.dapp_api;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DappApiApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
 }
