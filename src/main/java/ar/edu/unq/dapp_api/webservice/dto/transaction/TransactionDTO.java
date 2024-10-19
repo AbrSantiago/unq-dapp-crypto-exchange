3526d189b4072112d0294be0c26b4632c8b49baa
Santiago <santiago_abregu_@hotmail.com>
2024-10-19 20:45:38 -0300

    Changed LocalDate to LocalDateTime in TransactionDTO

--- src/main/java/ar/edu/unq/dapp_api/webservice/dto/transaction/TransactionDTO.java
+++ src/main/java/ar/edu/unq/dapp_api/webservice/dto/transaction/TransactionDTO.java
@@ -1,25 +1,22 @@
 package ar.edu.unq.dapp_api.webservice.dto.transaction;
 
-import ar.edu.unq.dapp_api.model.OperationIntent;
 import ar.edu.unq.dapp_api.model.Transaction;
 import ar.edu.unq.dapp_api.model.enums.TransactionStatus;
 import ar.edu.unq.dapp_api.webservice.dto.operationIntent.OperationIntentDTO;
 import ar.edu.unq.dapp_api.webservice.dto.user.SimpleUserDTO;
 import lombok.Getter;
-
-import java.time.LocalTime;
+import java.time.LocalDateTime;
 
 @Getter
 public class TransactionDTO {
-
     private final Long id;
     private final OperationIntentDTO operationIntent;
     private final SimpleUserDTO seller;
     private final SimpleUserDTO buyer;
     private final TransactionStatus status;
-    private final LocalTime startTime;
+    private final LocalDateTime startTime;
 
-    public TransactionDTO(Long id, OperationIntentDTO operationIntent, SimpleUserDTO seller, SimpleUserDTO buyer, TransactionStatus status, LocalTime startTime) {
+    public TransactionDTO(Long id, OperationIntentDTO operationIntent, SimpleUserDTO seller, SimpleUserDTO buyer, TransactionStatus status, LocalDateTime startTime) {
         this.id = id;
         this.operationIntent = operationIntent;
         this.seller = seller;
@@ -31,5 +28,4 @@ public class TransactionDTO {
     public static TransactionDTO fromModel(Transaction transaction) {
         return new TransactionDTO(transaction.getId(), OperationIntentDTO.fromModel(transaction.getOperationIntent()), SimpleUserDTO.fromModel(transaction.getSeller()), SimpleUserDTO.fromModel(transaction.getBuyer()), transaction.getStatus(), transaction.getStartTime());
     }
-
 }
